1dac7b116ee0bf618dae6d82416f2cbf9d202e43
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2025-05-31 19:08:18 +0600

    Get author info with articales

---
+++ README.md
@@ -0,0 +1,40 @@
+# The Buzz Bulletin 
+
+## The Buzz Bulletin is a online newpaper platform. 
+
+## Admin Credentials
+- Admin Username: Admin
+- Admin Email: admin@gmail.com
+- Admin Passoword: Admin@gmail.com1? 
+- Live site URL:  [https://the-buzz-bulletin.web.app/](https://the-buzz-bulletin.web.app/)
+
+## Key features of website:
+-  Dynamic Trending Articles:
+Ensures an engaging user experience by highlighting the most popular content.
+
+- Responsive Design:
+Offers seamless navigation and usability for all users.
+
+- Premium Features:
+Distinction between free and premium users with exclusive access to premium articles.
+
+- Advanced Authentication:
+Email and password-based authentication with password validation (capital letter, special character, numeric character).
+
+- Admin Functionality:
+Advanced management of articles and users (approval, declination with reason, delete, make premium).
+
+- Article Management:
+Users can create articles with detailed forms (title, image, tags, description, publisher).
+
+- Enhanced User Experience:
+Sweet alerts and toasts for all CRUD operations and successful authentication.
+
+- Subscription Modal:
+Encourages user engagement with premium features.
+
+- Pagination and Limits:
+Normal users are limited to one article unless they have a premium subscription.
+
+- SEO and Usability Enhancements:
+Website title reflects the site name dynamically with a favicon.
--- index.js
+++ index.js
@@ -16,180 +16,193 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+    const userCollection = client.db('newspaperDB').collection('users');
+    const publisherCollection = client.db('newspaperDB').collection('publishers');
+    const articleCollection = client.db('newspaperDB').collection('atricles');
 
-
-
-        const userCollection = client.db('newspaperDB').collection('users');
-        const publisherCollection = client.db('newspaperDB').collection('publishers');
-        const articleCollection = client.db('newspaperDB').collection('atricles');
-
-
-         // jwt related api
+    // jwt related api
     app.post('/jwt', async (req, res) => {
-        const user = req.body;
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-        res.send({ token });
-      })
-  
-      // middlewares 
-      const verifyToken = (req, res, next) => {
-        // console.log('inside verify token', req.headers.authorization);
-        if (!req.headers.authorization) {
-          return res.status(401).send({ message: 'unauthorized access' });
-        }
-        const token = req.headers.authorization.split(' ')[1];
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-          if (err) {
-            return res.status(401).send({ message: 'unauthorized access' })
-          }
-          req.decoded = decoded;
-          next();
-        })
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
       }
-  
-      // use verify admin after verifyToken
-      const verifyAdmin = async (req, res, next) => {
-        const email = req.decoded.email;
-        const query = { email: email };
-        const user = await userCollection.findOne(query);
-        const isAdmin = user?.role === 'admin';
-        if (!isAdmin) {
-          return res.status(403).send({ message: 'forbidden access' });
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
         }
+        req.decoded = decoded;
         next();
+      })
+    }
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    // User Realted API 
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'User already exists', insertedId: null })
       }
 
-        // User Realted API 
-        app.get('/users', async (req, res) => {
-            const result = await userCollection.find().toArray();
-            res.send(result);
-        })
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
 
-        app.post('/users', async (req, res) => {
-            const user = req.body;
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
 
+      const result = await userCollection.findOne({ email: email });
+      res.send(result);
+    })
 
-            const query = { email: user.email }
-            const existingUser = await userCollection.findOne(query);
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
-            if (existingUser) {
-                return res.send({ message: 'user already exists', insertedId: null })
-            }
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
 
-            const result = await userCollection.insertOne(user);
-            res.send(result);
-        })
+    //   Publisher realted API 
+    app.get('/publishers', async (req, res) => {
+      const result = await publisherCollection.find().toArray();
+      res.send(result);
+    })
 
-        app.patch('/users/admin/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const updatedDoc = {
-              $set: {
-                role: 'admin'
-              }
-            }
-            const result = await userCollection.updateOne(filter, updatedDoc);
-            res.send(result);
-          })
+    app.post('/publishers', async (req, res) => {
+      const publishers = req.body;
 
-        app.delete('/users/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await userCollection.deleteOne(query);
-            res.send(result);
-        })
+      // const query = { email: user.email }
+      // const existingUser = await userCollection.findOne(query);
 
-        //   Publisher realted API 
-        app.get('/publishers', async (req, res) => {
-            const result = await publisherCollection.find().toArray();
-            res.send(result);
-        })
+      // if (existingUser) {
+      //     return res.send({ message: 'user already exists', insertedId: null })
+      // }
 
-        app.post('/publishers', async (req, res) => {
-            const publishers = req.body;
+      const result = await publisherCollection.insertOne(publishers);
+      res.send(result);
+    })
 
-            // const query = { email: user.email }
-            // const existingUser = await userCollection.findOne(query);
-
-            // if (existingUser) {
-            //     return res.send({ message: 'user already exists', insertedId: null })
-            // }
-
-            const result = await publisherCollection.insertOne(publishers);
-            res.send(result);
-        })
-
-        app.delete('/publishers/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await publisherCollection.deleteOne(query);
-            res.send(result);
-        })
+    app.delete('/publishers/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await publisherCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
-        //   Articles realted API 
-        app.get('/articles', async (req, res) => {
-            const result = await articleCollection.find().toArray();
-            res.send(result);
-        })
+    //   Articles realted API 
+    app.get('/articles', async (req, res) => {
+      const result = await articleCollection.find().toArray();
 
-        app.post('/articles', async (req, res) => {
-            const publishers = req.body;
+      for (const author of result) {
+        const query1 = { _id: new ObjectId(author._id) }
 
-            // const query = { email: user.email }
-            // const existingUser = await userCollection.findOne(query);
+        const user = await userCollection.findOne(query1);
+        console.log(user);
+        if (user) {
+          author.name = user.name;
+          author.email = user.email;
+        }
+      }
 
-            // if (existingUser) {
-            //     return res.send({ message: 'user already exists', insertedId: null })
-            // }
+      res.send(result);
+    })
 
-            const result = await articleCollection.insertOne(publishers);
-            res.send(result);
-        })
+    app.post('/articles', async (req, res) => {
+      const publishers = req.body;
 
-        app.get('/articles/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
+      // const query = { email: user.email }
+      // const existingUser = await userCollection.findOne(query);
 
-            const result = await articleCollection.findOne(query);
-            res.send(result);
-        })
+      // if (existingUser) {
+      //     return res.send({ message: 'user already exists', insertedId: null })
+      // }
 
-        app.delete('/articles/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await articleCollection.deleteOne(query);
-            res.send(result);
-        })
+      const result = await articleCollection.insertOne(publishers);
+      res.send(result);
+    })
+
+    app.get('/articles/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+      const result = await articleCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.delete('/articles/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await articleCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
